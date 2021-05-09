0fd87b8f4ecb48e202568e785403fa8d59cf8bee
KHOA <khoavd@minori.com.vn>
2021-05-09 19:39:53 +0700

    state and lifecycle

--- src/index.js
+++ src/index.js
@@ -8,4 +8,52 @@ export class Hello extends React.Component {
   }
 }
 
-ReactDOM.render(<Hello />, document.getElementById("root"));
+// function Clock(props) {
+//   return (
+//     <div>
+//       <h1>Hello Win!</h1>
+//       <h2>It is {props.date.toLocaleTimeString()}</h2>
+//     </div>
+//   );
+// }
+
+// function Tick() {
+//   return <Clock date={new Date()} />;
+// }
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+
+  /*là một method lifecycle component
+    Được gọi sau khi render một component 
+    mounting là khi thêm một xử lý gì đó khi component
+    được render vào DOM lần đầu tiên.
+   */
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date(),
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello Win!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}</h2>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Clock />, document.getElementById("root"));
